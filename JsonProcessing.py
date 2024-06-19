1cea7e32b7ed1d5c886d151f28b5036671847527
Sebastian Karl <s.karl625@gmx.de>
2024-06-19 17:33:27 +0200

    Adjusted the write_testfile function to include the module.exports

--- JsonProcessing.py
+++ JsonProcessing.py
@@ -1,29 +1,38 @@
 import json
 
-def format_test(test:str):
-    line_break = test.find('{') + 1
-    formated_test = test[:line_break] +'\n'
-
-    old_line_break = line_break
-    line_break = test.find(';', old_line_break) + 1
-    while line_break > 0:
-        formated_test += '\t' + test[old_line_break : line_break] + '\n'
-        old_line_break = line_break
-        line_break = test.find(';', old_line_break) + 1
-
-    formated_test += test[len(test)-1] + '\n' + '\n'
-    return formated_test
+def extract_name(code:str):
+    name = code.split('const ')[1]
+    name = name.split(' ')[0]
+    return name
 
 def write_JSON(program_name: str, generated: str):
     with open(f'./data/JSON/{program_name}_Tests.json', 'w') as f:
         for line in generated:
             f.write(line)
 
-def write_Test(program_name: str, generated_json: str):
+def write_Testfile(program_name: str, generated_json: str):
     data = json.loads(generated_json)
-    with open(f'{program_name}_Tests.js', 'w') as f:
+    export = {}
+    with open(f'./data/Tests/{program_name}_Tests.js', 'w') as f:
         for item in data['tests']:
             test = item['code']
+            name = extract_name(test)
+            export[name] = item['name']
             f.write(test)
             f.write('\n'*2)
-            print(test)
+
+        f.write('module.exports = [\n')
+        for name in export:
+            f.write('\t{\n')
+            f.write(f'\t\t test: {name},\n')
+            f.write(f'\t\t name: "{name}",\n')
+            f.write(f'\t\t description: "{export[name]}",\n')
+            f.write(f'\t\t categories: []\n')
+            f.write('\t},\n')
+        f.write(']\n')
+
+if __name__ == '__main__':
+    response = ''
+    with open('./data/JSON/Archery_Tests.json', 'r') as f:
+        response = f.read()
+    write_Testfile('Archery', response)
