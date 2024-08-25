c558087c491cb14c610f44cc2fac778b612817ed
Sebastian Karl <s.karl625@gmx.de>
2024-08-25 17:41:42 +0200

    Baseline can now be generated with this script

--- TestExecutor.py
+++ TestExecutor.py
@@ -5,15 +5,16 @@ import argparse
 whisker_path = os.path.abspath('./whisker')
 solution_path = os.path.abspath('./data/SampleSolutions')
 tests_path = os.path.abspath('./data/Tests/js')
-results_path = os.path.abspath('./data/Results')
+results_path = os.path.abspath('./data/Results/csv')
 
-def generate_paths(program_name:str):
+def generate_paths(program_name:str, baseline:bool):
     if program_name is None or len(program_name) == 0:
         raise Error("Program name cannot be None or an empty string")
     paths = {}
+    sep = '/baseline/' if baseline else '/'
     paths['scratch_path'] = solution_path + '/' + program_name + '.sb3'
-    paths['tests_path'] = tests_path + '/' + program_name + '_Tests.js'
-    paths['csv_path'] = results_path + '/csv/' + program_name + '_Results.csv'
+    paths['tests_path'] = tests_path + sep + program_name + '_Tests.js'
+    paths['csv_path'] = results_path + sep + program_name + '_Results.csv'
     return paths
 
 def process_output(output:str):
@@ -23,20 +24,22 @@ def process_output(output:str):
     output = output[:-1]
     return output
 
-def build_command(program_name:str, csv:bool = True):
-    paths = generate_paths(program_name)
+def build_command(program_name:str, csv:bool = True, baseline:bool = False):
+    cmd = ['node', 'servant']
+    if baseline:
+        cmd.extend(['generate', \
+                    '-c', f'{whisker_path}/config/mio.json', \
+                    '-r', '10'])
+    else:
+        cmd.append('run')
 
-    cmd = ['node', 'servant', 'run']
-    cmd.append('-s')
-    cmd.append(f'{paths["scratch_path"]}')
-    cmd.append('-t')
-    cmd.append(f'{paths["tests_path"]}')
+    paths = generate_paths(program_name, baseline)
+    cmd.extend(['-s', f'{paths["scratch_path"]}', \
+                '-t', f'{paths["tests_path"]}'])
     if csv:
-        cmd.append('-v')
-        cmd.append(f'{paths["csv_path"]}')
-    cmd.append('-d')
-    cmd.append('-a')
-    cmd.append('10')
+        cmd.extend(['-v', f'{paths["csv_path"]}'])
+
+    cmd.extend(['-d', '-a', '10'])
     return cmd
 
 def execute_command(cmd:str, execution_path:str):
@@ -51,43 +54,49 @@ def get_tests():
         names[i] = names[i].strip()
     return names
 
-def run_all_tests():
+def run_all_tests(baseline:bool = False):
     testable_programs = get_tests()
     output = {}
     for program_name in testable_programs:
         print(f'Running tests for {program_name}')
-        cmd = build_command(program_name)
+        cmd = build_command(program_name, baseline)
         output[program_name] = execute_command(cmd, whisker_path)
     return output
 
-def run_test(program_name:str, generate_csv = False):
+def run_test(program_name:str, generate_csv:bool = False, baseline:bool=False):
     testable_programs = get_tests()
 
     if program_name not in testable_programs:
         raise Error(f"No program with name '{program_name}'!")
 
-    cmd = build_command(program_name, generate_csv)
+    cmd = build_command(program_name, generate_csv, baseline)
     return execute_command(cmd, whisker_path)
 
-def write_output(output):
+def write_output(output, baseline:bool = False):
+    sep = '/txt/whisker/' if baseline else '/txt/'
     for name in output.keys():
         lines = output[name].split('\\n')
-        with open(f'{results_path}/txt/{name}.txt', 'w') as f:
+        with open(f'{results_path}{sep}{name}.txt', 'w') as f:
             for line in lines:
                 f.write(f'{line}\n')
 
 def main():
     parser = argparse.ArgumentParser(description='Executes the whisker test(s) specified')
     parser.add_argument('-n', '--names', action='extend', type=str, nargs='+', help='names of the programs to test')
+    parser.add_argument('-b', dest='baseline', action='store_true', help='generates tests using WHISKERS built in MIO')
+    parser.set_defaults(baseline=False)
     names = parser.parse_args().names
+    baseline = parser.parse_args().baseline
+    print(baseline, type(baseline))
     output = {}
     if names is None:
-        output = run_all_tests()
+        pass
+        output = run_all_tests(baseline)
     else:
         for name in names:
             print(f'Running tests for {name}')
-            output[name] = run_test(name, True)
-    write_output(output)
+            output[name] = run_test(name, True, baseline)
+    write_output(output, baseline)
 
 if __name__ == '__main__':
     main()
